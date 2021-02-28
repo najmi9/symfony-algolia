01be32298e429e44dc5b57a0af7e1b0e088a2af2
najmi9 <imadnajmi9@gmail.com>
2021-02-28 09:39:25 -0500

    add throttle function

--- assets/algolia_search.js
+++ assets/algolia_search.js
@@ -1,24 +1,20 @@
 import algoliasearch from 'algoliasearch/lite';
 import cache from './cache';
-
+import throttle from './throttle';
 (function () {
-    document.querySelector('input#query').onkeyup = async e => {
-        e.preventDefault();
-        const q = e.currentTarget.value;
-
+    document.querySelector('input#query').onkeyup = throttle(async (event) => {
+        const q = event.target.value;
         if (q.length < 2) {
             return
         }
-
         const res = await search(q);
-
         showResults(res);
-    }
+    }, 500);
 
     async function search(query) {
         let key;
         let id;
-   
+
         if (await cache.get('algolia_search_key_only') === null) {
             console.log('here');
             const res = await fetch('/key');
@@ -26,10 +22,10 @@ import cache from './cache';
             key = r.key;
             id = r.id;
             cache.set('algolia_search_key_only', key);
-            cache.set('id',id);
+            cache.set('id', id);
         } else {
-            key =  await cache.get('algolia_search_key_only');
-            id =  await cache.get('id')
+            key = await cache.get('algolia_search_key_only');
+            id = await cache.get('id')
         }
 
         const requestOptions = {
@@ -47,21 +43,21 @@ import cache from './cache';
         results.hits.forEach(hit => {
             search = search + unit(hit);
         });
-        document.querySelector('h3#count').innerHTML = `${ results.nbHits } Results for "${ results.query }"`;
+        document.querySelector('h3#count').innerHTML = `${results.nbHits} Results for "${results.query}"`;
         document.querySelector('div#results').innerHTML = search;
     }
 
     const unit = hit => (`
         <div class="border-bottom">
             <strong>
-                ${ hit._highlightResult.name.value }
+                ${hit._highlightResult.name.value}
                 --
                 <b>
-                    ${ hit.price }
-                    $</b>
+                    ${hit.price} $
+                </b>
             </strong>
             <p>
-                ${ hit._highlightResult.description.value }
+                ${hit._highlightResult.description.value}
             </p>
         </div>
     `);
